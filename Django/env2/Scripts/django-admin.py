bae2d4c56fd571b81a4973b6ec6ab3ce01d60a40
tpietras <tomasz.a.pietras@gmail.com>
2015-10-29 04:17:06 +0100

    Create project

---
+++ Django/env2/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\tpietras\documents\visual studio 2015\Projects\Django\Django\env2\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
